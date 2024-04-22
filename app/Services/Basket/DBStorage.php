720691c000bd45891b4b22ad82c20ff9e8f67d24
mason.hows11 <mason.hows11@gmail.com>
2024-04-22 22:54:54 +0330

    add class for cart

---
+++ app/Services/Basket/DBStorage.php
@@ -0,0 +1,43 @@
+<?php
+namespace App\Services\Basket;
+
+use App\Services\Basket\Contracts\StorageInterface;
+
+class DBStorage implements StorageInterface , Countable
+{
+
+
+//// for get item
+    public function getItem($item){
+
+    }
+
+    //// for add item
+    public function addItem($item = null, $value){
+
+    }
+
+
+    //// for get all items
+    public function getAllItems(){
+
+    }
+
+    //// for check  item is exists or not
+    public function existsItem($item = null){
+
+    }
+
+    //// for delete item from
+    public function deleteItem($item = null){
+
+    }
+
+    //// for delete all items
+    public function deleteAllItems(){
+        
+    }
+
+
+
+}
