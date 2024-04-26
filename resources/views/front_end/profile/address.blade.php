a4a9529666ed839cd3d26d8e2aff0def1d90b835
mason.hows11 <mason.hows11@gmail.com>
2024-04-26 20:40:52 +0330

    address profile updated

--- resources/views/front_end/profile/address.blade.php
+++ resources/views/front_end/profile/address.blade.php
@@ -26,47 +26,7 @@
                     </div>
 
                     <div class="row">
-
-                        @forelse ( $addresses as $address)
-                        <div class="col-lg-6 col-md-12">
-                            <div class="card-horizontal-address">
-                                <div class="card-horizontal-address-desc">
-                                    <h4 class="card-horizontal-address-full-name">{{ $address->recipient_first_name . ' ' . $address->recipient_last_name }}</h4>
-                                    <p>
-                                        {{ $address->address_description }}
-                                    </p>
-                                </div>
-                                <div class="card-horizontal-address-data">
-                                    <ul class="card-horizontal-address-methods float-right">
-                                        <li class="card-horizontal-address-method">
-                                            <i class="mdi mdi-email-outline"></i>
-                                             استان: <span>{{   $address->province->name ? $address->province->name : ' - '  }}</span>
-                                        </li>
-                                        <li class="card-horizontal-address-method">
-                                            <i class="mdi mdi-email-outline"></i>
-                                            شهر : <span>{{ $address->city->name ? $address->city->name : ' - ' }}</span>
-                                        </li>
-                                        <li class="card-horizontal-address-method">
-                                            <i class="mdi mdi-email-outline"></i>
-                                            کدپستی : <span>{{  $address->postal_code }}</span>
-                                        </li>
-                                        <li class="card-horizontal-address-method">
-                                            <i class="mdi mdi-cellphone-iphone"></i>
-                                            تلفن همراه : <span>{{ $address->mobile != null ? $address->mobile : $address->user->mobile }}</span>
-                                        </li>
-                                    </ul>
-                                    <div class="card-horizontal-address-actions">
-
-                                        <button class="btn-note" data-toggle="modal"
-                                            data-target="#modal-location-edit">ویرایش</button>
-
-                                        <button class="btn-note" data-toggle="modal"
-                                            data-target="#remove-location">حذف</button>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        @endforeach
+                        <livewire:front.profile.address.index-address>
                     </div>
                 </div>
             </div>
