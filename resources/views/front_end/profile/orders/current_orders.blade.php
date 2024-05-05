6068ddbac3b2a9bf37ef21349a0a3b5b0f0d991b
mason.hows11 <mason.hows11@gmail.com>
2024-05-05 17:44:38 +0330

    add html template orders

--- resources/views/front_end/profile/orders/current_orders.blade.php
+++ resources/views/front_end/profile/orders/current_orders.blade.php
@@ -3,5 +3,49 @@
     سفارشات جاری
 @endsection
 @section('left_profile_content')
+<div class="col-xl-9 col-lg-8 col-md-8 col-sm-12">
+    <div class="row">
+        <div class="col-12">
+            <div
+                class="section-title text-sm-title title-wide mb-1 no-after-title-wide dt-sl mb-2 px-res-1">
+                <h2>همه سفارش‌ها</h2>
+            </div>
 
+            <div class="dt-sl">
+                <div class="table-responsive">
+                    <table class="table table-order">
+                        <thead>
+                            <tr>
+                                <th>#</th>
+                                <th>شماره سفارش</th>
+                                <th>تاریخ ثبت سفارش</th>
+                                <th>مبلغ قابل پرداخت</th>
+                                <th>مبلغ کل</th>
+                                <th>عملیات پرداخت</th>
+                                <th>جزییات</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+
+                            <tr>
+                                <td>1</td>
+                                <td>DDC-57456951</td>
+                                <td>۳۱ مرداد ۱۳۹۸</td>
+                                <td>۰</td>
+                                <td>۹,۹۸۹,۰۰۰ تومان</td>
+                                <td>لغو شده</td>
+                                <td class="details-link">
+                                    <a href="#">
+                                        <i class="mdi mdi-chevron-left"></i>
+                                    </a>
+                                </td>
+                            </tr>
+
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 @endsection
