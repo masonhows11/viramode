1a8b89c234091d649ee2492513332954b30f704a
mason.hows11 <mason.hows11@gmail.com>
2024-05-19 17:15:33 +0330

    category link parent & child style updated 80%

--- resources/views/front_end/category/child_categories.blade.php
+++ resources/views/front_end/category/child_categories.blade.php
@@ -2,7 +2,7 @@
     {{-- <ul class="mr-1">
          <li class=""> --}}
     <li class="mr-2 my-1">
-        <a href="{{ route('category', ['slug' => $subCategory->slug]) }}">{{ $subCategory->title_persian }}</a>
+        <a class="" href="{{ route('category', ['slug' => $subCategory->slug]) }}">{{ $subCategory->title_persian }}</a>
         <ul class="">
             @if ($subCategory->children != null)
                 @include('front_end.category.child_categories', ['category' => $subCategory->children])
