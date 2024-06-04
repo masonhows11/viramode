fc7f31bebec03f268f35a6418fb396f9e4a89cd6
mason.hows11 <mason.hows11@gmail.com>
2024-06-04 18:03:29 +0330

    defer loading not worked on style

--- app/Http/Livewire/Front/Layout/FrontMostVisited.php
+++ app/Http/Livewire/Front/Layout/FrontMostVisited.php
@@ -17,9 +17,9 @@ class FrontMostVisited extends Component
     public function render()
     {
         return view('livewire.front.layout.front-most-visited')
-            ->with(['products' => $this->readyToLoad ?
+            ->with(['products' =>
                 Product::select('id','title_persian','origin_price','slug','thumbnail_image')
                 ->orderByDesc('views')
-                ->take(6)->get() : [] , ]);
+                ->take(6)->get()  ]);
     }
 }
