02abed42b844cc57dd89f36592351f777881932c
mason.hows11 <mason.hows11@gmail.com>
2024-06-10 19:15:15 +0330

    custom slider banner slider style updated

--- resources/views/livewire/front/layout/front-custom-banner.blade.php
+++ resources/views/livewire/front/layout/front-custom-banner.blade.php
@@ -1,25 +1,35 @@
 <div>
+    <div class="row d-flex justify-content-center border border-2 border-danger my-5">
 
 
-    <div class="swiper mySwiper">
-        {{-- main slider content --}}
-        <div class="swiper-wrapper">
-          <div class="swiper-slide">Slide 1</div>
-          <div class="swiper-slide">Slide 2</div>
-          <div class="swiper-slide">Slide 3</div>
-          <div class="swiper-slide">Slide 4</div>
-          <div class="swiper-slide">Slide 5</div>
-          <div class="swiper-slide">Slide 6</div>
-          <div class="swiper-slide">Slide 7</div>
-          <div class="swiper-slide">Slide 8</div>
-          <div class="swiper-slide">Slide 9</div>
+        <div class="col-lg-12">
+
+            <div class="swiper mySwiper ">
+                {{-- main slider content --}}
+                <div class="swiper-wrapper">
+                  <div class="swiper-slide">Slide 1</div>
+                  <div class="swiper-slide">Slide 2</div>
+                  <div class="swiper-slide">Slide 3</div>
+                  <div class="swiper-slide">Slide 4</div>
+                  <div class="swiper-slide">Slide 5</div>
+                  <div class="swiper-slide">Slide 6</div>
+                  <div class="swiper-slide">Slide 7</div>
+                  <div class="swiper-slide">Slide 8</div>
+                  <div class="swiper-slide">Slide 9</div>
+                </div>
+
+                {{-- navigation slider --}}
+                <div class="swiper-button-next"></div>
+                <div class="swiper-button-prev"></div>
+
+              </div>
+
         </div>
 
-        {{-- navigation slider --}}
-        <div class="swiper-button-next"></div>
-        <div class="swiper-button-prev"></div>
+    </div>
 
-      </div>
+
+
 
 
 
