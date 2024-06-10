2aef93168a834c8d1471a3106e974b9cac67f3ff
mason.hows11 <mason.hows11@gmail.com>
2024-06-10 16:17:00 +0330

    add custom slider banner livewire cmp

---
+++ app/Http/Livewire/Front/Layout/FrontCustomBanner.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Front\Layout;
+
+use Livewire\Component;
+
+class FrontCustomBanner extends Component
+{
+    public function render()
+    {
+        return view('livewire.front.layout.front-custom-banner');
+    }
+}
