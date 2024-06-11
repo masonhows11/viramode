f70e2fcfbefd3e630e63a3a9d34530eea1c8059d
mason.hows11 <mason.hows11@gmail.com>
2024-06-11 17:17:39 +0330

    add condition for blade custom banner

--- resources/views/livewire/front/layout/front-custom-banner.blade.php
+++ resources/views/livewire/front/layout/front-custom-banner.blade.php
@@ -1,9 +1,6 @@
+@if ($banners->isNotEmpty())
 <div>
     <div class="row   my-5">
-
-
-
-
             <div dir="rtl"  class="swiper mySwiper py-5 col-lg-11 col-md-10 col-sm-6 col-10">
                 {{-- main slider content --}}
                 <div class="swiper-wrapper">
@@ -19,44 +16,8 @@
                   @endforeach
                 </div>
                 <div class="swiper-pagination"></div>
-
-                {{-- navigation slider --}}
-                {{--
-                 <div class="swiper-button-next"></div>
-                 <div class="swiper-button-prev"></div>
-                --}}
             </div>
-
-
-
-
-
     </div>
-
-
-
-
-
-
-           {{-- <div class="col-12">
-                <div class="">
-                    @foreach ($banners as $banner)
-                    <div class="item " style="height:320px">
-
-                        <a class="product-thumb" href="{{ $banner->link }}">
-                            @if ($banner->path && \Illuminate\Support\Facades\Storage::disk('public')->exists($banner->path))
-                                <img class="rounded img-thumbnail"
-                                    src="{{ asset('storage/' . $banner->path) }}"
-                                    alt="Product Thumbnail">
-                            @else
-                                <img class="rounded" src="{{ asset('default_image/no-image-icon-23494.png') }}"
-                                    alt="Product Thumbnail">
-                            @endif
-                        </a>
-
-                    </div>
-                    @endforeach
-                </div>
-            </div> --}}
-
 </div>
+@endif
+
